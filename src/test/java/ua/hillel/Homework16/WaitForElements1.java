a422a51b444b8f7fdbb8cede40912028030fbeb5
mariagladij <kokina0101@gmail.com>
2023-12-18 22:55:17 +0100

    first commit

---
+++ src/test/java/ua/hillel/Homework16/WaitForElements1.java
@@ -0,0 +1,73 @@
+package ua.hillel.Homework16;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
+
+public class WaitForElements1 {
+    private static WebDriver driver;
+
+    @BeforeClass
+    static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading");
+    }
+
+    @AfterClass
+    static void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void waitHelloWorld() {
+        FluentWait<WebDriver> fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(StaleElementReferenceException.class);
+
+        WebElement waitHW = driver.findElement(By.linkText("Example 1: Element on page that is hidden"));
+        waitHW.click();
+
+        WebElement clickStart = driver.findElement(By.xpath("//button[text()='Start']"));
+        clickStart.click();
+
+        WebElement text = fluentWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("finish")));
+        Assert.assertEquals(text.getText(), "Hello World!");
+
+    }
+
+    @Test
+    public void waitHelloWorldTwo() {
+        FluentWait<WebDriver> fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(StaleElementReferenceException.class);
+
+        String url = "https://the-internet.herokuapp.com/dynamic_loading";
+        driver.get(url);
+
+        WebElement waitHW = driver.findElement(By.linkText("Example 2: Element rendered after the fact"));
+        waitHW.click();
+
+        WebElement clickStart = driver.findElement(By.xpath("//button[text()='Start']"));
+        clickStart.click();
+
+        WebElement text = fluentWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("finish")));
+        Assert.assertEquals(text.getText(), "Hello World!");
+
+    }
+
+}
---
+++ src/test/java/ua/hillel/Homework16/WhatInPage.java
@@ -0,0 +1,51 @@
+package ua.hillel.Homework16;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import java.time.Duration;
+
+
+
+public class WhatInPage {
+    private static WebDriver driver;
+
+    @BeforeClass
+    static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
+    }
+
+    @AfterClass
+    static void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void checkFirstButton() throws InterruptedException {
+        WebElement clickABox = driver.findElement(By.id("adder"));
+        clickABox.click();
+        Thread.sleep(3000);
+        WebElement findSquare = driver.findElement(By.id("box0"));
+        String actualColor = findSquare.getCssValue("color");
+        String expectedColor = "rgba(0, 0, 0, 1)";
+        Assert.assertEquals(expectedColor, actualColor);
+    }
+
+    @Test
+    public void checkSecondButton() throws InterruptedException{
+        WebElement clickReveal = driver.findElement(By.id("reveal"));
+        clickReveal.click();
+        Thread.sleep(3000);
+        WebElement findInputField = driver.findElement(By.id("revealed"));
+        findInputField.sendKeys("I want to sleep!");
+
+    }
+
+}
