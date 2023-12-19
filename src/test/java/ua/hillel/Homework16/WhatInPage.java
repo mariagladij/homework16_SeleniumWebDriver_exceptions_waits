df2a88ab50468a35eeaa58a113159c5af87db28c
mariagladij <kokina0101@gmail.com>
2023-12-19 16:04:17 +0100

    new commit

--- src/test/java/ua/hillel/Homework16/WhatInPage.java
+++ src/test/java/ua/hillel/Homework16/WhatInPage.java
@@ -5,13 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
 import java.time.Duration;
-
-
 
 public class WhatInPage {
     private static WebDriver driver;
@@ -27,23 +26,28 @@ public class WhatInPage {
     static void tearDown() {
         driver.quit();
     }
+
     @Test
-    public void checkFirstButton() throws InterruptedException {
+    public void checkFirstButton() {
         WebElement clickABox = driver.findElement(By.id("adder"));
         clickABox.click();
-        Thread.sleep(3000);
-        WebElement findSquare = driver.findElement(By.id("box0"));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement findSquare = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("box0")));
         String actualColor = findSquare.getCssValue("color");
         String expectedColor = "rgba(0, 0, 0, 1)";
         Assert.assertEquals(expectedColor, actualColor);
     }
 
     @Test
-    public void checkSecondButton() throws InterruptedException{
+    public void checkSecondButton() {
         WebElement clickReveal = driver.findElement(By.id("reveal"));
         clickReveal.click();
-        Thread.sleep(3000);
-        WebElement findInputField = driver.findElement(By.id("revealed"));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement findInputField = wait.until(ExpectedConditions.elementToBeClickable(By.id("revealed")));
         findInputField.sendKeys("I want to sleep!");
     }
 }
